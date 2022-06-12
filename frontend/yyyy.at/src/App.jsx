0a8847ad3ae220c75b2a91910c89e83e01f49119
Abhinav Diddee <adiddee@live.com>
2022-06-12 01:40:00 -0700

    test emoji bar

---
+++ frontend/yyyy.at/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import EmojiBar from './components/EmojiBar' 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <EmojiBar emojis='😂😂😂'/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/yyyy.at/src/components/EmojiBar.jsx
@@ -0,0 +1,11 @@
+function EmojiBar(props) {
+    return (
+        <div className="EmojiBar">
+            <p>
+                {props.emojis}
+                .yyyy.at
+            </p>
+        </div>
+    )
+}
+export default EmojiBar;
