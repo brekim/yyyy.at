c1cdf4e20b8ffae4374fb63ba63f193b99fd26e7
Abhinav Diddee <adiddee@live.com>
2022-06-12 16:13:50 -0700

    Auto stash before merge of "master" and "origin/master"

--- frontend/yyyy.at/src/components/EmojiBar.jsx
+++ frontend/yyyy.at/src/components/EmojiBar.jsx
@@ -1,33 +1,16 @@
 
 import React, { Component }  from 'react';
 
-import Picker from 'emoji-picker-react';
+function EmojiBar(props){
+    return (
+        <div>
+            <p>
+                {props.emojis}
+                .yyyy.at
+            </p>
+        </div>
+    )
 
-class EmojiBar extends React.Component {
-    constructor(props) {
-        super(props);
-        // Don't call this.setState() here!
-        this.state = { emojis : "" };
-    }
-
-    addemoji(emoji) {
-        let newe = this.state.emojis.concat(emoji)
-        this.setState({
-            emojis: newe
-          });
-    }
-    render() {
-        return (
-            <div>
-                <p>
-                    {this.state.emojis}
-                    .yyyy.at
-                </p>
-                <Picker onEmojiClick={function(){}} pickerStyle={{ width: '100%' }} />
-            </div>
-        )
-
-    }
     
 }
 export default EmojiBar;
