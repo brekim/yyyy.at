fed727cf2696d48762f5d14a0578ba9b79297d00
Abhinav Diddee <adiddee@live.com>
2022-06-12 01:18:03 -0700

    app generated

---
+++ frontend/yyyy.at/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={require('./log.png')} className="App-logo" alt="logo" />
+        <p>
+          welcome to yyyy.at
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          get your yyyy.at
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
