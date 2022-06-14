585ab7478c685ffa4a7ae8ee9aa2bfcc91d93ad3
brekim <brettnakim@gmail.com>
2022-06-13 23:46:01 -0700

    set up project for firebase

---
+++ src/components/EmojiBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component }  from 'react';
+
+function EmojiBar(props){
+    return (
+        <div>
+            <p>
+                {props.emojis}
+                .yyyy.at
+            </p>
+        </div>
+    )
+
+    
+}
+export default EmojiBar;
